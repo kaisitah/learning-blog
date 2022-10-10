b5cf152be7f1ffaadd24c291cce26882cac701ff
Kaisa Haverinen <kaisa.haverinen@iki.fi>
2022-10-10 08:10:10 +0300

    Change Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 const Button = (props) => {
     function handleClick(event) {
         console.log("The button was clicked");
-        alert("The button was clicked!");
+        //alert("The button was clicked!");
     }
     return (
-        <button className='Button' onClick={handleClick}>Button</button>
+        <button className='Button' onClick={handleClick}>Search</button>
     )
 }
 
